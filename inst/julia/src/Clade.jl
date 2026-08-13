5669c29987a842a722bc350f06aab35e5a2b35dc
Bhavya Jain <148748644+b1805@users.noreply.github.com>
2026-08-13 09:25:33 -0400

    feat: terminate simulation early on population extinction

--- inst/julia/src/Clade.jl
+++ inst/julia/src/Clade.jl
@@ -452,7 +452,16 @@ function run_clade(user_specs::AbstractDict)
         if verbose && t % 100 == 0
             @info "tick $t: $(length(env.agents)) agents alive"
         end
-    end
+        
+        # If there are no agents and no predators left alive, there is no
+        # reason to continue ticking the simulation.
+        if isempty(env.agents) && isempty(env.predators)
+            if verbose
+                @info "tick $t: population extinct, terminating early"
+            end
+            break
+        end
+    end # end of `for t in 1:max_t` loop
 
     _env_to_result(env)
 end
